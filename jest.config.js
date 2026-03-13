877b739804de7afd0bfe5a6442622ddf2fdafcb9
Kaname Sudo <mikan.paku89@gmail.com>
2026-03-13 12:02:26 +0900

    testファイル修正

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ export default {
   // テスト実行前に読み込むセットアップファイルを指定
   setupFilesAfterEnv: ["./jest.setup.ts"],
   // テスト対象のファイルパターンを指定。__tests__フォルダ内の.spec.ts/.spec.tsxのみ対象にする
+  testMatch: ["**/__tests__/**/*.spec.(ts|tsx)"],
   transform: {
     // .ts/.tsxファイルをts-jestで変換する
     // デフォルトではtsconfig.jsonが読み込まれるが、Viteプロジェクトではモジュール方式の違いでエラーが出るため
@@ -14,6 +15,7 @@ export default {
       "ts-jest",
       {
         tsconfig: "tsconfig.jest.json",
+        diagnostics: false,
       },
     ],
   },
